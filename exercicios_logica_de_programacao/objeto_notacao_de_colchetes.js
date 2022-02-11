ef58bce054abd3413e34a8a86a6c2ae2935065f4
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-02-11 20:22:07 -0300

    Add files via upload

---
+++ exercicios_logica_de_programacao/objeto_notacao_de_colchetes.js
@@ -0,0 +1,9 @@
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  const playerNumber = 16;  
+  const player = testObj[playerNumber];   
+  console.log(player);
